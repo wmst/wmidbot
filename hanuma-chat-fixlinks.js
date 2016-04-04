75420fb0869d1cee2e509cd509f1484e63e0a975
Kazakov <alekseykass@gmail.com>
2016-04-04 14:18:01 +0300

    admin commit

--- hanuma-chat-fixlinks.js
+++ hanuma-chat-fixlinks.js
@@ -3,7 +3,7 @@ $(function(){
 		var bt = $('.gallery_cover_list li .startchatbutton');
 		$.each(bt,function(i,v){
 			var cl = $(v).attr('onclick').split(',');
-			cl = cl[0].replace("popup=window.open('",'').replace("'",'');
+			cl = cl[0].replace("openHCLiveChatWindow('",'').replace("'",'');
 			$(v).after('<a href="'+cl+'" target="_blank" class="startchatbutton" style="display: block;text-align: center;">open chat</a>');
 			$(v).remove();	
 		});
